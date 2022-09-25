066f815595fae3c4e7ea72ea55cec280d0167e98
hfccr <hfccr@outlook.com>
2022-09-25 21:57:06 +0530

    Add ui changes

--- packages/react-app/components/Splash/index.js
+++ packages/react-app/components/Splash/index.js
@@ -10,17 +10,18 @@ export function Splash({ web3Modal }) {
     }
   }
   return (
-    <Container maxWidth="xl">
-      <Stack direction="column" justifyContent="center" alignItems="center">
+    <Container maxWidth="xl" sx={{ padding: 4 }}>
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
         <Typography variant="h1">RM3</Typography>
-        <Typography variant="h4">A Web3 CRM For Web3</Typography>
-        <Typography variant="h4">Chains: SKALE, Optimism, Polygon, Cronos, Aurora</Typography>
+        <Typography variant="h4">
+          RM3 is an awesome serverless Web3 CRM for building and developing DAO, DeFi and other communities based
+          completely on web3 technologies. It also introduces a novel protocol to help dApp developers to make
+          serverless apps.
+        </Typography>
+        <Typography variant="h4">BYOS Protocol: Ceramic DIDs & Compose DB</Typography>
         <Typography variant="h4">Communication: EPNS, XMTP</Typography>
-        <Typography variant="h4">Data Providers: The Graph, Covalent, QuickNode</Typography>
-        <Typography variant="h4">Oracles: UMA, Tellor</Typography>
-        <Typography variant="h4">Social: Lens</Typography>
-        <Typography variant="h4">Benefits: Worldcoin, Superfluid, ENS, Unstoppable Domains</Typography>
-        <Typography variant="h4">Economics: AAVE, Yearn, APWine</Typography>
+        <Typography variant="h4">Data Providers: The Graph, Covalent, ENS, NFTPort</Typography>
+        <Typography variant="h4">Chains: Optimism, Polygon, Aurora</Typography>
         {showLaunch && <Link href="/dapp">Launch App</Link>}
       </Stack>
     </Container>
--- packages/react-app/pages/_app.js
+++ packages/react-app/pages/_app.js
@@ -46,9 +46,9 @@ function MyApp({ Component, pageProps }) {
                       href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏗</text></svg>"
                     />
                   </Head>
-                  <NetworkDisplay />
-                  <DevUI />
-                  <ThemeSwitch />
+                  {/* <NetworkDisplay /> */}
+                  {/* <DevUI /> */}
+                  {/* <ThemeSwitch /> */}
                   {/* Page Header start */}
                   <HeaderContainer {...pageProps} />
                   {/* Page Header end */}
